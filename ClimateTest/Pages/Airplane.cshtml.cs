d6032e2c39c44ba4bba33c851602751fd12baee8
tjm00023 <tjm00023@mix.wvu.edu>
2024-02-06 14:11:16 -0500

    Add project files.

---
+++ ClimateTest/Pages/Airplane.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ClimateTest.Pages
+{
+    public class AirplaneModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
